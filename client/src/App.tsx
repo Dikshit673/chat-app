d89352d7df459426e3a27e7b3d151196f946725b
dikshit673 <sachinnavodian@gmail.com>
2025-11-03 10:13:31 +0530

    client init

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import '@/css/index.css';
+// import { RootRouter } from '@/router/RootRouter';
+// import { useSocketIoClient } from '@/hooks/useSocketIoClient';
+// import { useSession } from './hooks/useSession';
+
+function App() {
+  // useSession();
+
+  // useSocketIoClient();
+
+  return (
+    <div>hello</div>
+  )
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,21 @@
+
+import { createRoot } from 'react-dom/client';
+import '@/css/index.css';
+
+
+
+
+import App from './App.tsx';
+import { Toaster } from 'react-hot-toast';
+
+// import { Provider } from 'react-redux';
+// import { store } from '@/redux/store.ts';
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
+    <Toaster />
+  </>
+);
