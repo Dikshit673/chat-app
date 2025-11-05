7bd50e15bb4fd73f048b2f832d1d8a08390ca60d
dikshit673 <sachinnavodian@gmail.com>
2025-11-05 19:10:12 +0530

    Add allContacts  com and uses grid for online  and search contacts.

--- client/src/features/contacts/components/Sidebar/Sidebar.tsx
+++ client/src/features/contacts/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,16 @@
 import { Card } from '@/components/ui';
 import OnlineContacts from './components/OnlineContacts';
 import SearchContacts from './components/SearchContacts';
+import AllContacts from './components/AllContacts';
 
 const Sidebar = () => {
   return (
-    <aside className="size-full">
+    <aside className="size-full overflow-hidden">
       <Card className="size-full">
-        <div className="h-full space-y-2">
+        <div className="grid h-full grid-cols-1 grid-rows-[auto_auto_1fr] gap-2">
           <OnlineContacts />
           <SearchContacts />
+          <AllContacts />
         </div>
       </Card>
     </aside>
--- client/src/features/contacts/components/Sidebar/components/SearchContacts.tsx
+++ client/src/features/contacts/components/Sidebar/components/SearchContacts.tsx
@@ -1,15 +1,21 @@
 import { Heading } from '@/components/ui';
 
+const SearchForm = () => {
+  return (
+    <form action="">
+      <input
+        type="text"
+        className="w-full rounded-lg border border-gray-500 px-3 py-1"
+      />
+    </form>
+  );
+};
+
 const SearchContacts = () => {
   return (
-    <div className="space-y-1 px-4">
+    <div className="grid h-full grid-cols-1 grid-rows-[auto_1fr] gap-2 px-4">
       <Heading.H6 title="search contacts" />
-      <form action="">
-        <input
-          type="text"
-          className="w-full rounded-lg border border-gray-500 px-3 py-1"
-        />
-      </form>
+      <SearchForm />
     </div>
   );
 };
