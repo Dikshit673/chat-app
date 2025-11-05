eb6f9314d1c4dc02e4cf4acdde51843cb431dfba
dikshit673 <sachinnavodian@gmail.com>
2025-11-05 23:33:17 +0530

    messageItem to chatItem

--- client/src/features/chat/components/ChatBox/components/ChatBody.tsx
+++ client/src/features/chat/components/ChatBox/components/ChatBody.tsx
@@ -4,7 +4,7 @@ import { formatTime } from '@/utils/formatDateAndTime';
 import { cva } from 'class-variance-authority';
 import { useMemo } from 'react';
 
-const messageWrapperCVA = cva('flex items-end rounded-xl p-2', {
+const chatWrapperCVA = cva('flex items-end rounded-xl p-2', {
   variants: {
     isOwnerMessage: {
       true: 'bg-prime-100 justify-self-end rounded-br-none',
@@ -13,32 +13,26 @@ const messageWrapperCVA = cva('flex items-end rounded-xl p-2', {
   },
 });
 
-type MessageItemProps = {
-  message: (typeof messagesDummyData)[0];
+type ChatItemProps = {
+  chat: (typeof messagesDummyData)[0];
 };
 
-const MessageItem = ({ message }: MessageItemProps) => {
+const ChatItem = ({ chat }: ChatItemProps) => {
+  const { _id, senderId, image, text, createdAt } = chat;
   const authUser = userDummyData[0];
   const isOwnerMessage = useMemo(
-    () => message.senderId === authUser._id,
-    [message.senderId, authUser._id]
+    () => senderId === authUser._id,
+    [senderId, authUser._id]
   );
-  const formatedTime = useMemo(
-    () => formatTime(message.createdAt),
-    [message.createdAt]
-  );
-  console.log({ id: authUser._id, sender: message.senderId, isOwnerMessage });
+  const formatedTime = useMemo(() => formatTime(createdAt), [createdAt]);
+  console.log({ id: authUser._id, sender: senderId, isOwnerMessage });
   return (
-    <div className={cn(messageWrapperCVA({ isOwnerMessage }))}>
+    <div className={cn(chatWrapperCVA({ isOwnerMessage }))}>
       <div className="inline-flex flex-col gap-2">
-        {message.image && (
-          <img
-            src={message.image}
-            alt={message._id}
-            className="w-50 rounded-lg object-cover"
-          />
+        {image && (
+          <img src={image} alt={_id} className="w-50 rounded-lg object-cover" />
         )}
-        {message.text && <p>{message.text}</p>}
+        {text && <p>{text}</p>}
         <span className="text-end text-[0.75rem] font-normal text-gray-500">
           {formatedTime}
         </span>
@@ -50,10 +44,10 @@ const MessageItem = ({ message }: MessageItemProps) => {
 const ChatBody = () => {
   return (
     <div className="bg-prime-200 grid h-full grid-cols-1 gap-4 overflow-hidden">
-      <div className="slide-thin h-full space-y-2 overflow-y-auto px-5 py-5">
+      <div className="slide-thin h-full space-y-2 overflow-y-auto px-5 py-4">
         {/* <div className="flex flex-col gap-2"> */}
-        {messagesDummyData.map((message) => {
-          return <MessageItem key={message._id} message={message} />;
+        {messagesDummyData.map((chat) => {
+          return <ChatItem key={chat._id} chat={chat} />;
         })}
         {/* </div> */}
       </div>
