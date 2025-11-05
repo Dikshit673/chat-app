3e4e4ecc7748d2b4e5493217d1ec3e496839527d
dikshit673 <sachinnavodian@gmail.com>
2025-11-05 23:16:39 +0530

    update chat and added barrel import for chatbox

--- client/src/app/auth/Chat.tsx
+++ client/src/app/auth/Chat.tsx
@@ -1,7 +1,8 @@
-import { ChatCard } from '@/features/chat/components/ChatCard';
+import ChatBox from '@/features/chat/components/ChatBox';
+// import { NoChat } from '@/features/chat/components/NoChat';
 
 const Chat = () => {
-  return <ChatCard />;
+  return <ChatBox />;
 };
 
 export default Chat;
