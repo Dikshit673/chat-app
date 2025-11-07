a1c5830e27080402f54cd0643f8a677f517367f7
dikshit673 <sachinnavodian@gmail.com>
2025-11-07 23:11:22 +0530

    {🗄️]:Added auth token utilities

---
+++ server/src/features/auth/utils/tokens.ts
@@ -0,0 +1,56 @@
+import type { Response } from 'express';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import { ACC_JWT_CONFIG, REF_JWT_CONFIG } from '../configs/jwtConfig.js';
+import { sendError } from '@/utils/sendResponse.js';
+import type { IUserNoPassDocument } from '@/models/user.model.js';
+
+type UserVerifiedPayload = JwtPayload & IUserNoPassDocument;
+
+const extractUser = (
+  decodedPayload: UserVerifiedPayload
+): IUserNoPassDocument => {
+  const { iat, exp, nbf, jti, aud, iss, sub, ...userPayload } = decodedPayload;
+  return userPayload;
+};
+
+export const generateAccessToken = (payload: IUserNoPassDocument): string => {
+  return jwt.sign(payload, ACC_JWT_CONFIG.secret, ACC_JWT_CONFIG.options);
+};
+
+export const generateRefreshToken = (payload: IUserNoPassDocument): string => {
+  return jwt.sign(payload, REF_JWT_CONFIG.secret, REF_JWT_CONFIG.options);
+};
+
+const verifyAccessToken = (token: string): UserVerifiedPayload => {
+  return jwt.verify(token, ACC_JWT_CONFIG.secret) as UserVerifiedPayload;
+};
+
+const verifyRefreshToken = (token: string): UserVerifiedPayload => {
+  return jwt.verify(token, REF_JWT_CONFIG.secret) as UserVerifiedPayload;
+};
+
+export const handleAccessToken = (res: Response, token: string) => {
+  try {
+    const payload = verifyAccessToken(token);
+    const user = extractUser(payload);
+    return user;
+  } catch (error) {
+    const errMsg =
+      (error as Error).message || 'something wrong with access token.';
+    sendError(res, 401, errMsg);
+    return null;
+  }
+};
+
+export const handleRefreshToken = (res: Response, token: string) => {
+  try {
+    const payload = verifyRefreshToken(token);
+    const user = extractUser(payload);
+    return user;
+  } catch (error) {
+    const errMsg =
+      (error as Error).message || 'something wrong with access token.';
+    sendError(res, 403, errMsg);
+    return null;
+  }
+};
