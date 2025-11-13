7cba764eb940365f3c4e9b7a8b92bf60c84331ab
dikshit673 <sachinnavodian@gmail.com>
2025-11-13 22:05:02 +0530

    🗄️: update register logic

--- server/src/features/auth/controllers/register.ts
+++ server/src/features/auth/controllers/register.ts
@@ -1,25 +1,41 @@
-import { UserModel } from '@/models/user.model.js';
-import { sendError, sendSuccess } from '@/utils/sendResponse.js';
 import { Request, Response } from 'express';
+import { User } from '@/models/user.model.js';
+import { sendError, sendSuccess } from '@/utils/sendResponse.js';
+import { SALT_ROUNDS } from '@/constants/bcrypt.constant.js';
+import { generateSalt, hashPassword } from '@/lib/bcrypt.js';
+import { registerSchema } from '@/lib/zod.js';
 
 export const register = async (req: Request, res: Response) => {
-  const { name, email, password, profilePic = '' } = req.body;
-  if (name || email || password)
-    return sendError(res, 400, 'Invalid credentials.');
+  const registerData = registerSchema.safeParse(req.body);
+  if (!registerData.success) {
+    const errorMsg =
+      registerData.error.issues[0].message || 'invalid credentials';
+    return sendError(res, 400, errorMsg);
+  }
+  const { name, email, password, profilePic } = registerData.data;
 
   try {
-    const isEmailExist = await UserModel.findOne({ email });
-    if (isEmailExist) return sendError(res, 400, 'Email already exist.');
-    const user = await UserModel.create({
+    const isUserExistWithEmail = await User.findByEmail(email);
+    if (isUserExistWithEmail) {
+      return sendError(res, 400, 'Email already exist.');
+    }
+
+    const salt = await generateSalt(SALT_ROUNDS);
+    const hashedPassword = await hashPassword(password, salt);
+
+    const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       profilePic,
       role: 'user',
     });
-    return sendSuccess(res, 201, 'User created successfully.', { user });
+    if (!user) {
+      return sendError(res, 500, 'Something went wrong while creating user.');
+    }
+    return sendSuccess(res, 201, 'User created successfully.');
   } catch (error) {
-    const err = error as Error;
-    return sendError(res, 500, err.message);
+    const errorMsg = (error as Error).message || 'Something went wrong.';
+    return sendError(res, 500, errorMsg);
   }
 };
