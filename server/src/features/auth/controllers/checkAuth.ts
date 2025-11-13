f7d6e0b5146508fff7eb31782b3dd7ec9c961332
dikshit673 <sachinnavodian@gmail.com>
2025-11-13 22:07:05 +0530

    🗄️: write check-auth controller.

--- server/src/features/auth/controllers/checkAuth.ts
+++ server/src/features/auth/controllers/checkAuth.ts
@@ -1,6 +1,19 @@
-import { sendSuccess } from '@/utils/sendResponse.js';
+// import { authUserSchema } from '@/lib/zod.js';
+import { sendError, sendSuccess } from '@/utils/sendResponse.js';
 import { Request, Response } from 'express';
 
-export const checkAuth = async (_req: Request, res: Response) => {
-  return sendSuccess(res, 200, 'me route');
+export const checkAuth = async (req: Request, res: Response) => {
+  try {
+    // console.log('check auth');
+    const user = req.user;
+    if (!user) {
+      return sendError(res, 400, 'User not found.');
+    }
+    return sendSuccess(res, 200, 'Authenticated successfully', {
+      authUser: user,
+    });
+  } catch (error) {
+    const errorMsg = (error as Error).message || 'Something went wrong.';
+    return sendError(res, 500, errorMsg);
+  }
 };
