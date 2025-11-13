36fa26b54a18611bdd17e00a3c94f43b8d6b16e9
dikshit673 <sachinnavodian@gmail.com>
2025-11-13 22:06:39 +0530

    🗄️: write refresh controller

--- server/src/features/auth/controllers/refresh.ts
+++ server/src/features/auth/controllers/refresh.ts
@@ -1,6 +1,34 @@
-import { sendSuccess } from '@/utils/sendResponse.js';
+import { refreshTokenCookieSchema } from '@/lib/zod.js';
+import { EnvVars } from '@/utils/EnvVarConfig.js';
+import { sendError, sendSuccess } from '@/utils/sendResponse.js';
 import { Request, Response } from 'express';
+import { getRefreshTokenUser, issueAccessToken } from '../utils/tokens.js';
 
-export const refresh = async (_req: Request, res: Response) => {
-  return sendSuccess(res, 200, 'refresh route');
+export const refresh = async (req: Request, res: Response) => {
+  const cookieToken = req.cookies[EnvVars.REF_COOKIE_NAME];
+  const parsedCookies = refreshTokenCookieSchema.safeParse(cookieToken);
+
+  if (!parsedCookies.success) {
+    const err = parsedCookies.error.issues[0];
+    const message = EnvVars.IS_DEV ? err.message : 'Forbidden';
+    return sendError(res, 403, message);
+  }
+  const refreshToken = parsedCookies.data;
+  try {
+    const refreshData = getRefreshTokenUser(refreshToken);
+    if (!refreshData.success) {
+      const errorMsg = refreshData.error.message || 'Refresh token is invalid.';
+      return sendError(res, 403, errorMsg);
+    }
+
+    const refreshUserPayload = refreshData.data.user;
+    const renewedAccessToken = issueAccessToken(refreshUserPayload);
+
+    return sendSuccess(res, 200, 'refreshed auth token successfully', {
+      jwtToken: renewedAccessToken,
+    });
+  } catch (error) {
+    const errorMsg = (error as Error).message || 'Something went wrong.';
+    return sendError(res, 500, errorMsg);
+  }
 };
